94570563d3864eeee5b60ab35f3661e35020b3bc
Saoud Ahmed Khan <60979682+saoud-ahmed-khan@users.noreply.github.com>
2021-01-27 18:08:59 +0500

    headerCompleted

---
+++ src/Components/Header.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export const Header : React.FC = () => {
+    return (
+        <div className="header-section">
+            <h2 className="header-content">Quiz App</h2>
+        </div>
+    )
+}
