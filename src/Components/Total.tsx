c9a7b307ede7622b67c181a59ba8d5f05c758e02
Saoud Ahmed Khan <60979682+saoud-ahmed-khan@users.noreply.github.com>
2021-01-28 15:43:35 +0500

    adding style

--- src/Components/Total.tsx
+++ src/Components/Total.tsx
@@ -3,13 +3,13 @@ import React from 'react'
 export const Total: React.FC = () => {
   return (
     <div className="Total-section">
-      <div>
+      <div className="Total-sub">
         <div>Total Catogories</div>
-        <div>24</div>
+        <div className="Total-digits">24</div>
       </div>
-      <div>
+      <div className="Total-sub2">
         <div>Total Question</div>
-        <div>2000+</div>
+        <div className="Total-digits">2000+</div>
       </div>
     </div>
   )
