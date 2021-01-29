3a59e327789b0a9485529f365f8bf38ddd70b4ae
Saoud Ahmed Khan <60979682+saoud-ahmed-khan@users.noreply.github.com>
2021-01-29 23:04:23 +0500

    adding logos

--- src/App.tsx
+++ src/App.tsx
@@ -13,7 +13,17 @@ function App() {
     <div className="app">
       <Header/>
       <Total/>
+      <Categories logo="GiPerspectiveDiceSixFacesRandom" Catname="General Knowledge"/>
+      <Categories logo="MdComputer" Catname="Computer"/>
+      <Categories logo="GiMaterialsScience" Catname="Science & Nature"/>
+      <Categories logo="ImBook" Catname="Books"/>
       <Categories logo="GiAmericanFootballHelmet" Catname="Games"/>
+      <Categories logo="AiFillCar" Catname="Vehicals"/>
+
+
+
+
+
      
     </div>
   );
--- src/Components/Categories.tsx
+++ src/Components/Categories.tsx
@@ -2,6 +2,18 @@ import React from 'react'
 import { Category } from "./Types";
 import { IconContext } from 'react-icons'
 import { GiAmericanFootballHelmet } from "react-icons/gi";
+import { ImBook } from "react-icons/im";
+import { GiPerspectiveDiceSixFacesRandom } from "react-icons/gi";
+import { MdComputer } from "react-icons/md";
+import { GiMaterialsScience } from "react-icons/gi";
+import { AiFillCar } from "react-icons/ai";
+
+
+
+
+
+
+
 
 
 
@@ -11,9 +23,14 @@ export const Categories: React.FC<Category> = ({Catname,logo}) => {
         <div>
             <div>
                 <IconContext.Provider value={{ color: '#e68330', size: '50px' }}>
-                    {
-                        logo==="GiAmericanFootballHelmet" ? <GiAmericanFootballHelmet/> :null
-                    }
+                    {logo==="GiAmericanFootballHelmet" ? <GiAmericanFootballHelmet/> :null}
+                    {logo==="ImBook"? <ImBook/>:null}
+                    {logo==="GiPerspectiveDiceSixFacesRandom" ? <GiPerspectiveDiceSixFacesRandom/> :null}
+                    {logo==="MdComputer" ? <MdComputer/> :null}
+                    {logo==="GiMaterialsScience" ? <GiMaterialsScience/> :null}
+                    {logo==="AiFillCar" ? <AiFillCar/> :null}
+
+
                 </IconContext.Provider>
             </div>
     <div>{Catname}</div>
